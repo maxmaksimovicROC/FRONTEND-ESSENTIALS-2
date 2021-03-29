142fdad0195f0f30c4acb73b949ff5ccd104d8db
maxmaksimovic <maxmaksimovicroc@gmail.com>
2021-03-29 12:10:02 +0200

    aef

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,13 @@
-let strNaam;
-let boolNaamKlopt;
+
+var strNaam = prompt("Hoe heet je?"); 
+
+
+alert("Je heet " + strNaam); 
+
+var boolNaamCorrect = confirm("Heet Je Echt!" + strNaam +"?");
+
+if (confirm("Weet je het heel zeker ls je liegt heb je een probleem !")) {
+    txt = "je bent wie je zegt dat je bent";
+  } else {
+    txt = "kom terug met je echte naam";
+  }
